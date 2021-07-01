40ee88057e2201a559cc04af9c98b76bcab494e0
Harmelodic <matt@harmelodic.com>
2021-07-01 21:39:17 +0200

    Adding google provider and required terraform version

--- provider.tf
+++ provider.tf
@@ -1,6 +1,19 @@
 terraform {
+  required_version = ">=1.0.0"
+
   backend "gcs" {
     bucket = "harm-personal-projects-terraform-state"
     prefix = "ops"
   }
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.74.0"
+    }
+  }
+}
+
+provider "google" {
+  region = "europe-north1"
 }
