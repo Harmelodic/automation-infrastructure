b2850f64e31117e99e28091235a568ac1c60176a
Harmelodic <matt@harmelodic.com>
2021-07-05 21:41:25 +0200

    Splitting node pool from gke

--- src/gke.tf
+++ src/gke.tf
@@ -63,49 +63,6 @@ resource "google_container_cluster" "gke_cluster" {
   }
 }
 
-resource "google_container_node_pool" "gke_node_pool" {
-  cluster            = google_container_cluster.gke_cluster.name
-  initial_node_count = 1
-  location           = var.gke_location
-  name               = terraform.workspace
-  node_count         = 1
-  node_locations     = var.gke_node_locations
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    disk_size_gb    = 100
-    disk_type       = "pd-standard"
-    image_type      = "cos_containerd"
-    labels          = {
-      environment = terraform.workspace
-    }
-    local_ssd_count = 0
-    machine_type    = var.gke_node_pool_machine_type
-    metadata        = {
-      disable-legacy-endpoints = true
-    }
-    preemptible     = false
-    service_account = google_service_account.gke_node_pool.email
-    tags            = [
-      terraform.workspace
-    ]
-
-    shielded_instance_config {
-      enable_integrity_monitoring = true
-      enable_secure_boot          = true
-    }
-  }
-
-  upgrade_settings {
-    max_surge       = 2
-    max_unavailable = 1
-  }
-}
-
 resource "google_compute_network" "gke" {
   auto_create_subnetworks         = false
   delete_default_routes_on_create = false
@@ -131,33 +88,9 @@ resource "google_compute_subnetwork" "gke" {
   }
 }
 
-resource "google_service_account" "gke_node_pool" {
-  account_id   = "${terraform.workspace}-node-pool"
-  description  = "The default service account for pods to use"
-  display_name = "GKE Node Pool Service Account"
-}
-
-resource "google_project_iam_member" "gke_node_pool" {
-  member = "serviceAccount:${google_service_account.gke_node_pool.email}"
-  role   = "roles/viewer"
-}
-
-variable "gke_node_pool_machine_type" {
-  default     = "g1-small"
-  description = "Machine size for GKE node pool"
-  sensitive   = true
-  type        = string
-}
-
 variable "gke_location" {
   default     = "europe-north1"
   description = "Location of where GKE cluster will be created"
   sensitive   = true
   type        = string
-}
-
-variable "gke_node_locations" {
-  description = "The list of zones in which the node pool's nodes should be located"
-  sensitive   = true
-  type        = list(string)
 }
