e5012428217a2dd43ebbef46d5868072c46e6a11
Harmelodic <matt@harmelodic.com>
2021-08-11 23:02:51 +0200

    Database WIP and upgrade

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=1.0.0"
+  required_version = ">=1.0.3"
 
   backend "gcs" {
     bucket = "harmelodic-terraform-state"
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.74.0"
+      version = "3.79.0"
     }
   }
 }
