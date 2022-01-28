d4a6404d6d01d0a784b518024a988778d459146d
Harmelodic <matt@harmelodic.com>
2022-01-28 22:01:41 +0100

    Bash formatting

--- getting-started/terraform-service-account.sh
+++ getting-started/terraform-service-account.sh
@@ -4,33 +4,30 @@ PROJECT_ID=$2
 SERVICE_ACCOUNT_NAME=terraform
 FULL_SERVICE_ACCOUNT_IDENTIFIER=$SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com
 
-if [ -n "$1" ] && [ -n "$2" ];
-then
-	if [ "$1" == "create" ];
-	then
-		gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
-			--project="$PROJECT_ID"
+if [ -n "$1" ] && [ -n "$2" ]; then
+    if [ "$1" == "create" ]; then
+        gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
+            --project="$PROJECT_ID"
 
-		gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-			--member=serviceAccount:"$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
-			--role=roles/editor \
-			--project="$PROJECT_ID"
+        gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+            --member=serviceAccount:"$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
+            --role=roles/editor \
+            --project="$PROJECT_ID"
 
-		gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-			--member=serviceAccount:"$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
-			--role=roles/resourcemanager.projectIamAdmin \
-			--project="$PROJECT_ID"
+        gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+            --member=serviceAccount:"$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
+            --role=roles/resourcemanager.projectIamAdmin \
+            --project="$PROJECT_ID"
 
-		gcloud iam service-accounts keys create service_account.json \
-			--iam-account="$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
-			--project="$PROJECT_ID"
+        gcloud iam service-accounts keys create service_account.json \
+            --iam-account="$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
+            --project="$PROJECT_ID"
 
-		echo "Put this in the appropriate GitLab CI/CD variable:"
+        echo "Put this in the appropriate GitLab CI/CD variable:"
 
-		cat service_account.json
-	elif [ "$1" == "delete" ];
-	then
-		gcloud iam service-accounts delete "$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
-			--project="$PROJECT_ID"
-	fi
+        cat service_account.json
+    elif [ "$1" == "delete" ]; then
+        gcloud iam service-accounts delete "$FULL_SERVICE_ACCOUNT_IDENTIFIER" \
+          --project="$PROJECT_ID"
+    fi
 fi
--- getting-started/terraform-state-bucket.sh
+++ getting-started/terraform-state-bucket.sh
@@ -4,19 +4,16 @@ PROJECT_ID=$2
 BUCKET_NAME=$3
 REGION=$4
 
-if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ] && [ -n "$4" ];
-then
-	if [ "$1" == "create" ];
-	then
-		gsutil mb \
-			-p "$PROJECT_ID" \
-			-c STANDARD \
-			-l "$REGION" \
-			-b on \
-			gs://"$BUCKET_NAME"
-	elif [ "$1" == "delete" ];
-	then
-		gsutil rb \
-			gs://"$BUCKET_NAME"
-	fi
+if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ] && [ -n "$4" ]; then
+    if [ "$1" == "create" ]; then
+        gsutil mb \
+            -p "$PROJECT_ID" \
+            -c STANDARD \
+            -l "$REGION" \
+            -b on \
+            gs://"$BUCKET_NAME"
+    elif [ "$1" == "delete" ]; then
+        gsutil rb \
+            gs://"$BUCKET_NAME"
+    fi
 fi
