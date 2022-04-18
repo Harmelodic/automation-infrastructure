698a7faa6c4be617ae93e678fea19004ad62c3de
Harmelodic <matt@harmelodic.com>
2022-04-18 12:06:04 +0200

    Giving automation more permissions for handling basic billing and organisational stuff

--- src/automation-service-account.tf
+++ src/automation-service-account.tf
@@ -5,8 +5,15 @@ resource "google_service_account" "automation" {
   description  = "Harmelodic Organisation Automation Service Account"
 }
 
-resource "google_organization_iam_member" "automation_organisation_editor" {
+resource "google_organization_iam_member" "automation_organisation_perms" {
+  for_each = toset([
+    "roles/billing.projectManager",
+    "roles/billing.viewer",
+    "roles/editor",
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.organizationViewer",
+  ])
   member = "serviceAccount:${google_service_account.automation.email}"
   org_id = data.google_organization.harmelodic_com.org_id
-  role   = "roles/editor"
+  role   = each.key
 }
