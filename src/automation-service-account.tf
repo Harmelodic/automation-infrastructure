cc9d66039ec6af1b49402d46e56396f4e2b55caf
Harmelodic <matt@harmelodic.com>
2024-07-10 19:32:56 +0200

    Move service account permissions to service account

--- src/automation-service-account.tf
+++ src/automation-service-account.tf
@@ -4,3 +4,13 @@ resource "google_service_account" "automation" {
   display_name = "automation"
   description  = "Harmelodic Organisation Automation Service Account"
 }
+
+# Allows automation account to be used as an identity for any GitHub workload.
+# Though only workloads that meet all the criteria:
+# - Repositories owned by Harmelodic
+# - Using the `main` branch (meaning other branches/refs can't provision infrastructure)
+resource "google_service_account_iam_member" "automation_workload_identity_user" {
+  service_account_id = google_service_account.automation.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.owner_and_branch/Harmelodic::branch::refs/heads/main"
+}
--- src/automation-workload-identity-federation-github.tf
+++ src/automation-workload-identity-federation-github.tf
@@ -41,13 +41,3 @@ resource "google_iam_workload_identity_pool_provider" "automation_github" {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
 }
-
-# Allows automation account to be used as an identity for any GitHub workload.
-# Though only workloads that meet all the criteria:
-# - Repositories owned by Harmelodic
-# - Using the `main` branch (meaning other branches/refs can't provision infrastructure)
-resource "google_service_account_iam_member" "automation_workload_identity_user" {
-  service_account_id = google_service_account.automation.id
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.owner_and_branch/Harmelodic::branch::refs/heads/main"
-}
